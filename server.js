aa87b00a87d3e692d04d8b27b31bdb4fb293f150
Newton <narmstrongv@gmail.com>
2021-07-21 18:13:12 -0700

    added handlebars/connections

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+// const path = require('path');
+// const express = require('express');
+// //const session = require('express-session');
+// const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 
 
-app.use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static(path.join(__dirname, 'public')))
 
 //add secrect-session stuff
 
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers});
+// const hbs = exphbs.create({ helpers});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers'));
+//app.use(require('./controllers'));
 
 //turn on db, sync w/sequelize
 sequelize.sync({ force: false }).then(() => {
